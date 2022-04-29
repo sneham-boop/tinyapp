42952531fb93c63e3eb004a9b074cf3f15e0eebf
Sneha Mahajan <sneh.km@gmail.com>
2022-04-29 19:29:28 -0400

    Add post route and random string generator function.

--- express_server.js
+++ express_server.js
@@ -2,16 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generaterandomstring = () => {
+  let randStr = "";
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for (let i = 0; i < 6; i++) {
+    const randChar = Math.floor(Math.random() * chars.length);
+    randStr = randStr.concat(chars[randChar]);
+  }
+  return randStr;
+};
+
+generaterandomstring();
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 // Middleware
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,7 +40,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -35,11 +49,17 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
 
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.status(200).send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}!`);
