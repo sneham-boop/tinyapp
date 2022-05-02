ff8757b26580346ad8fe85bfbf7b72e91c2b10a0
Sneha Mahajan <sneh.km@gmail.com>
2022-05-02 12:42:45 -0400

    Clean up formatting in express_server.js

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
